003798e48831251f3ce4bb63f52e8305ae1f8040
name <123867179+agnijeetsharma@users.noreply.github.com>
2025-03-28 17:06:51 +0530

    refactor: move RedisPubSub setup to a separate module

--- GraphQL/src/graphql/resolvers/resolvers.ts
+++ GraphQL/src/graphql/resolvers/resolvers.ts
@@ -4,33 +4,13 @@
 //     asyncIterator<T>(triggers: string | string[]): AsyncIterableIterator<T>;
 //   }
 // }
-import { RedisPubSub } from 'graphql-redis-subscriptions';
-import Redis from 'ioredis';
-
-// Define Redis connection options
-const options: Redis.RedisOptions = {
-  host: '127.0.0.1',
-  port: 6379,
-  retryStrategy: (times: number) => Math.min(times * 50, 2000),
-};
-
-// Explicitly type the Redis instances
-const publisher = new Redis(options);
-const subscriber = new Redis(options);
-
-// Properly type the RedisPubSub instance
-const pubsub: RedisPubSub = new RedisPubSub({
-  publisher,
-  subscriber,
-});
-
-export { pubsub };
 
 
 
 import Usertype, { User } from "../../models/userModel.js";
 // import { PubSub } from "graphql-subscriptions";
 import { getUsers, getUserById } from "../../controllers/userController.js";
+import {pubsub} from "../subscriptions/redis-subscription.js"
 import {
   getAllCourses,
   getCourseById,
---
+++ GraphQL/src/graphql/subscriptions/redis-subscription.ts
@@ -0,0 +1,21 @@
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import Redis from 'ioredis';
+
+// Define Redis connection options
+const options: Redis.RedisOptions = {
+  host: '127.0.0.1',
+  port: 6379,
+  retryStrategy: (times: number) => Math.min(times * 50, 2000),
+};
+
+// Explicitly type the Redis instances
+const publisher = new Redis(options);
+const subscriber = new Redis(options);
+
+// Properly type the RedisPubSub instance
+const pubsub: RedisPubSub = new RedisPubSub({
+  publisher,
+  subscriber,
+});
+
+export { pubsub };
